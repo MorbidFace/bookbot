4523ca24240ac78dbb422f85933930c18fb1ecd8
MorbidFace <sean.wwilsondev@gmail.com>
2024-04-12 18:05:20 +0800

    finished book report

--- main.py
+++ main.py
@@ -26,9 +26,29 @@ def get_character_count(text):
 def report(words, character_count, book_path):
     print(f"--- Begin report of {book_path} ---")
     print(f"{len(words)} words found in the document")
+    dict_list = make_dict_list(character_count)
+    dict_list.sort(reverse=True, key=sort_on)
+    print()
+    for char in dict_list:
+        print(f"The '{char["char"]}' character was found {char["count"]} times")
+    print("--- End report ---")
 
-# def sort_on(dict):
-    # return dict[]
+def sort_on(dict):
+    return dict["count"]
+
+def make_dict_list(dict):
+    dict_list = []
+    alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x','y','z'}
+    character_set = set(alphabet)
+    for char in dict:
+        if (char in character_set):
+            char_dict = {
+                "char": char,
+                "count": dict[char]
+            }
+            dict_list.append(char_dict)
+        
+    return dict_list
 
 
 main()
